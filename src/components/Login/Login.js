7fa1b8e1f6bf32fd39479855f2af9b26895f8f98
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-28 20:13:53 +0600

    updated styles on login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,13 +36,13 @@ const Login = () => {
                 <Grid container component="main" justifyContent="center"  alignItems="center" style={{ minHeight: '100vh' }} >
                     <CssBaseline />
                     <Grid item xs={12} sm={11} md={6} lg={5} component={Paper} elevation={6} square >
-                        <TabsUnstyled defaultValue={0}>
+                        <TabsUnstyled defaultValue={0} style={{background: "#eff3f6"}} >
                             <TabsList>
                                 <Tab>Sign in</Tab>
                                 <Tab>Sign up</Tab>
                             </TabsList>
                             <TabPanel value={0}>
-                                <Box sx={{ my: 8, mx: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Box sx={{ py: 8, px: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                                     <Typography component="h1" variant="h5" sx={{textTransform: 'uppercase', fontWeight: "500"}} >
                                         Sign in
                                     </Typography>
@@ -62,7 +62,7 @@ const Login = () => {
                                 </Box>
                             </TabPanel>
                             <TabPanel value={1}>
-                                <Box sx={{ mt: 3, mb: 2, mx: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                <Box sx={{ pt: 3, pb: 2, px: 4, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                                     <Typography component="h1" variant="h5" sx={{textTransform: 'uppercase', fontWeight: "500"}} >
                                         Sign up
                                     </Typography>
@@ -72,12 +72,9 @@ const Login = () => {
                                     <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                                         <TextField margin="normal" required id="name" label="Full Name" type="name" name="name" autoComplete="off" autoFocus sx={{marginRight: "20px"}} InputLabelProps={{style: {fontSize: "14px"}}} />
                                         <TextField margin="normal" required id="number" label="Phone Number" type="number" name="number" autoComplete="off" InputLabelProps={{style: {fontSize: "14px"}}} />
-                                        
                                         <TextField margin="normal" required id="birthDate" label="Date Of Birth" type="date" name="birthDate" defaultValue={currentDate} sx={{marginRight: "20px"}} InputLabelProps={{shrink: true, style: {fontSize: "14px"}}} />
                                         <TextField margin="normal" required id="gender" label="Gender" type="text" name="gender" autoComplete="off"  InputLabelProps={{style: {fontSize: "14px"}}} />
-
                                         <TextField margin="normal" required fullWidth id="email" label="Email" type="email" name="email" autoComplete="off" InputLabelProps={{style: {fontSize: "14px"}}} />
-
                                         <TextField margin="normal" required name="password" label="Password ddddd" type="password" id="password" sx={{marginRight: "20px"}} inputProps={{autoComplete: 'new-password', form: {autoComplete: 'off'}}} InputLabelProps={{style: {fontSize: "14px"}}} />
                                         <TextField margin="normal" required name="confirm_pass" label="Confirm Password" type="password" id="confirm_pass" inputProps={{autoComplete: 'new-password', form: {autoComplete: 'off'}}} InputLabelProps={{style: {fontSize: "14px"}}} />
                                         <Box sx={{textAlign: 'center'}} >
