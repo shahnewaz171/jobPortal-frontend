d2053d7a98c4a1b500dfcb6d00713635e8a04aec
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-30 18:03:07 +0600

    added validations and  make a post request on create job

--- src/components/Home/CreateJob/CreateJob.js
+++ src/components/Home/CreateJob/CreateJob.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { FaPlusCircle } from "react-icons/fa";
-import { Typography, Button, Select, MenuItem, Box, Modal, TextField, TextareaAutosize } from '@mui/material';
+import { Typography, Button, Select, MenuItem, Box, Modal, TextField, TextareaAutosize, FormControl, FormHelperText } from '@mui/material';
+import { useForm, Controller } from "react-hook-form";
 import { GiCancel } from "react-icons/gi";
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
 import { Div, modalStyle } from '../../shared/custom-styles';
 import './CreateJob.css';
 
@@ -9,37 +12,60 @@ const CreateJob = ({ open, setOpen }) => {
     const [shift, seShift] = useState('');
     const [department, setDepartment] = useState('');
     const [level, setLevel] = useState('');
-    const [vacancy, setVacancy] = useState('');
-    const [salary, setSalary] = useState('');
-    const [question, setQuestion] = useState('');
+    const [jobType, setJobType] = useState('');
+    const [disable, setDisable] = useState(false);
+    const { register, handleSubmit, watch, reset, formState: { errors }, control } = useForm({
+        mode: "all",
+        reValidateMode: 'onChange'
+    });
 
     const handleShift = (e) => {
         seShift(e.target.value);
+        console.log(shift);
     };
     const handleDepartment = (e) => {
         setDepartment(e.target.value);
+        console.log(department);
     };
     const handleLevel = (e) => {
         setLevel(e.target.value);
+        console.log(level);
     };
-    const handleFilterQue = (e) => {
-        setQuestion(e.target.value);
-    };
-    const handleVacancy = (e) => {
-        setVacancy(e.target.value);
-    };
-    const handleSalary = (e) => {
-        setSalary(e.target.value);
+    const handleJobType = (e) => {
+        setJobType(e.target.value);
+        console.log(jobType);
     };
 
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const onSubmit = (data) => {
+        console.log(data);
+        const jwtToken = localStorage.getItem('jwtToken') || null;
+
+        if(jwtToken !== null){
+           setTimeout(() => {
+            axios.post('https://tf-practical.herokuapp.com/api/job_post/', data, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`
+                }
+            })
+            .then(res => {
+                if (res) {
+                    console.log(res.statusText);
+                    reset();
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+           }, 2000);
+        }
+    };
 
     return (
         <>
             <Div>
                 <Typography variant="p" component="div" className="createJob-btn">
-                    <Button onClick={handleOpen} variant="contained" startIcon={<FaPlusCircle />} >
+                    <Button onClick={() => setOpen(true)} variant="contained" startIcon={<FaPlusCircle />} >
                         create job
                     </Button>
                 </Typography>
@@ -47,12 +73,12 @@ const CreateJob = ({ open, setOpen }) => {
             <Modal
                 keepMounted
                 open={open}
-                onClose={handleClose}
+                onClose={() => setOpen(false)}
                 aria-labelledby="keep-mounted-modal-title"
                 aria-describedby="keep-mounted-modal-description"
                 className="createJob-modal"
             >
-                <Box sx={modalStyle}>
+                <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={modalStyle}>
                     <Div className="filterInput">
                         <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
                             Create Job
@@ -61,73 +87,96 @@ const CreateJob = ({ open, setOpen }) => {
                             <Typography variant="p" component="div" className="searchItem-title" >
                                 Job Title
                             </Typography>
-                            <TextField fullWidth />
+                            <TextField fullWidth name="jobTitle" {...register("jobTitle", { required: "This field is required" })} error={Boolean(errors.jobTitle)} helperText={errors.jobTitle?.message} />
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
-                                Shift
+                                LastDateOfApply
                             </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={shift} onChange={handleShift} >
-                                <MenuItem value="Morning">Morning</MenuItem>
-                                <MenuItem value="Evening">Evening</MenuItem>
-                                <MenuItem value="Night">Night</MenuItem>
-                            </Select>
-                        </Div>
-                        <Div className="createJob-item">
-                            <Typography variant="p" component="div" className="searchItem-title" >
-                                Department
-                            </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={department} onChange={handleDepartment} >
-                                <MenuItem value="IT">IT</MenuItem>
-                                <MenuItem value="Non IT">Non IT</MenuItem>
-                                <MenuItem value="General">General</MenuItem>
-                            </Select>
+                            <TextField type="date" fullWidth name="lastDateOfApply" {...register("lastDateOfApply", { required: "This field is required" })} error={Boolean(errors.lastDateOfApply)} helperText={errors.lastDateOfApply?.message} />
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
                                 Level
                             </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={level} onChange={handleLevel} >
-                                <MenuItem value="Junior">Junior</MenuItem>
-                                <MenuItem value="Mid">Mid</MenuItem>
-                                <MenuItem value="Senior">Senior</MenuItem>
-                            </Select>
+                            <TextField fullWidth name="level" {...register("level", { required: "This field is required" })} error={Boolean(errors.level)} helperText={errors.level?.message} />
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
-                                Vacancy
+                                Shift
                             </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={vacancy} onChange={handleVacancy} >
-                                <MenuItem value="3">3</MenuItem>
-                                <MenuItem value="5">5</MenuItem>
-                                <MenuItem value="8">8</MenuItem>
-                            </Select>
+                            <FormControl fullWidth error={Boolean(errors.shift)} >
+                                <Controller 
+                                    render={({ field }) => (
+                                        <Select {...field} sx={{ border: "1px solid #707070" }} >
+                                        <MenuItem value="day">Day</MenuItem>
+                                        <MenuItem value="night">Night</MenuItem>
+                                        </Select>
+                                    )}
+                                    name="shift"
+                                    control={control}
+                                    defaultValue=""
+                                    rules={{
+                                        required: "This field is required"
+                                    }}
+                                />
+                                <FormHelperText>{errors.shift?.message}</FormHelperText>
+                            </FormControl>
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
-                                Salary
+                                Location
                             </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={salary} onChange={handleSalary} >
-                                <MenuItem value="20k">20k</MenuItem>
-                                <MenuItem value="30k">30k</MenuItem>
-                                <MenuItem value="40k">40k</MenuItem>
-                            </Select>
+                            <TextField  fullWidth name="location" {...register("location", { required: "This field is required" })} error={Boolean(errors.location)} helperText={errors.location?.message} />
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
-                                Filter Question
+                                Vacancies
                             </Typography>
-                            <Select fullWidth sx={{ border: "1px solid #707070" }} value={question} onChange={handleFilterQue} >
-                                <MenuItem value="Lorem ipsum dolor sit1">Lorem ipsum dolor sit1</MenuItem>
-                                <MenuItem value="Lorem ipsum dolor si2">Lorem ipsum dolor sit2</MenuItem>
-                                <MenuItem value="Lorem ipsum dolor sit3">Lorem ipsum dolor sit3</MenuItem>
-                            </Select>
+                            <TextField type="number" fullWidth name="vacancies" {...register("vacancies", { required: "This field is required" })} error={Boolean(errors.vacancies)} helperText={errors.vacancies?.message} />
+                        </Div>
+                        <Div className="createJob-item">
+                            <Typography variant="p" component="div" className="searchItem-title" >
+                                Job type
+                            </Typography>
+                            <FormControl fullWidth error={Boolean(errors.jobType)} >
+                                <Controller 
+                                    render={({ field }) => (
+                                        <Select {...field} sx={{ border: "1px solid #707070" }} >
+                                            <MenuItem value="part_time">Part Time</MenuItem>
+                                            <MenuItem value="full_time">Full Time</MenuItem>
+                                            <MenuItem value="internship">Internship</MenuItem>
+                                        </Select>
+                                    )}
+                                    name="jobType"
+                                    control={control}
+                                    defaultValue=""
+                                    rules={{
+                                        required: "This field is required"
+                                    }}
+                                />
+                                <FormHelperText>{errors.jobType?.message}</FormHelperText>
+                            </FormControl>
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" >
                                 Job Description
                             </Typography>
-                            <TextareaAutosize aria-label="minimum height" minRows={8} placeholder="Write job description..." style={{ width: "100%", padding: "10px" }} />
+                            <FormControl fullWidth error={Boolean(errors.jobDescription)}>
+                                <Controller 
+                                    render={({ field }) => (
+                                        <TextareaAutosize {...field} aria-label="minimum height" minRows={8} placeholder="Write job description..." style={{ width: "100%", padding: "10px" }}  />
+                                    )}
+                                    name="jobDescription"
+                                    control={control}
+                                    defaultValue=""
+                                    rules={{
+                                        required: "This field is required"
+                                    }}
+                                />
+                                <FormHelperText>{errors.jobDescription?.message}</FormHelperText>
+                            </FormControl>
+                          
                         </Div>
                         <Div className="createJob-item">
                             <Typography variant="p" component="div" className="searchItem-title" />
@@ -141,11 +190,14 @@ const CreateJob = ({ open, setOpen }) => {
                             </Div>
                         </Div>
                         <Typography variant="p" component="p" className="close-btn">
-                            <GiCancel onClick={handleClose} />
+                            <GiCancel onClick={() => setOpen(false)} />
                         </Typography>
                     </Div>
                 </Box>
             </Modal>
+            {
+                <ToastContainer />
+            }
         </>
     );
 };
