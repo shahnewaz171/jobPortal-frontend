008fbaaa99818cf3d9de476dfa153c6e529e849a
shahnewaz601 <shahnewaz601@gmail.com>
2021-12-30 22:49:38 +0600

    added not found route

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
           <Route path="edit-job/:id" 
             element={ <PrivateRoute> <EditJob /> </PrivateRoute> } 
           />
+          <Route path='*' element={<PrivateRoute> <Home /> </PrivateRoute>} />
         </Routes>
       </BrowserRouter>
     </>
